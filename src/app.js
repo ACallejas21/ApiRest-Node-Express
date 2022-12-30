51c3b06893070d1f1ed665c00e7b0a58b1502e2c
ACallejas21 <akdiel.callejas21@gmail.com>
2022-12-30 15:52:43 -0600

    prueba para pagina 404

--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@ import express from "express";
 import empleadosRoutes from "./routes/empleados.routes.js";
 import indexRoutes from "./routes/index.routes.js";
 import './config.js'
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
 
 app.use(express.json())
+//app.use(express.static("src"))
 app.use(indexRoutes) 
 app.use('/rest/',empleadosRoutes)
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use((req,res,next) => {
     res.status(404).json({
-        mensaje:  "Endpoint not found"
+        mensaje: "ENDPOINT NOT FOUND"
     })
 }) 
 
